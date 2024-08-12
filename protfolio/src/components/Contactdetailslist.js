3c449230e817ae173c003e40359c459d5a293124
Vijay pradeep <vijaypradeep3568@gmail.com>
2024-08-12 22:12:37 +0530

    add the contactlist

---
+++ protfolio/src/components/Contactdetailslist.js
@@ -0,0 +1,25 @@
+import {FaAddressCard } from 'react-icons/fa';
+import {FaPhoneAlt } from 'react-icons/fa';
+export const Contactdetails = [
+    {
+        detailname:"Address",
+         subdetail1:"2/138,Nadu street",         
+         subdetail2:"Mallayapuram, Dindigu;-624707",
+         symbol: <FaAddressCard/>       
+
+    },
+    {
+        detailname:"Phone",
+        subdetail1:"9342496489",         
+        subdetail2:"7667311060",
+        symbol: <FaPhoneAlt/>       
+
+    },
+    {
+        detailname:"Email",
+        subdetail1:"pradeepvijay3568@gmail.com",         
+        subdetail2:"vijaypradeep3568@gmail.com",
+        symbol: <FaAddressCard/>       
+
+    }
+]
